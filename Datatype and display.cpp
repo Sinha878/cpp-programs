ac907dc50c8d95a7ed1e011c306b3133d02b409a
Sinha878 <64796137+Sinha878@users.noreply.github.com>
2021-04-04 13:22:49 +0530

    Create Datatype and display.cpp

---
+++ Datatype and display.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+//Displaying a statement in the console application	
+	cout<<"hello world"<<endl<<endl<<endl;
+	//Details of all data types available in c and c++
+	cout<<"float :"<<sizeof(float)<<endl;
+	cout<<"int :" <<sizeof(int)<<endl;
+	cout<<"unsigned int:" <<sizeof(unsigned int)<<endl;
+	cout<<"signed int:"<<sizeof(signed int)<<endl;
+	cout<<"long :"<<sizeof(long)<<endl;
+	cout<<"short :"<<sizeof(short)<<endl;
+	cout<<"float :"<<sizeof(float)<<endl;
+	cout<<"double :"<<sizeof(double)<<endl;
+	cout<<"char :"<<sizeof(char)<<endl;
+	cout<<"bool :"<<sizeof(bool)<<endl;
+//Maximum and minimum value of intiger	
+	cout<<INT_MAX<<endl;
+	cout<<INT_MIN<<endl;
+	system("pause>0");
+	
+}
