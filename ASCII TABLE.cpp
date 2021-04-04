339ddad7465cd5bbefc720482e522fdb80dd59ad
Sinha878 <64796137+Sinha878@users.noreply.github.com>
2021-04-04 13:23:46 +0530

    Create ASCII TABLE.cpp

---
+++ ASCII TABLE.cpp
@@ -0,0 +1,16 @@
+//program for ASCII table
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i1,i2,i3,i4,i5;
+	char c1,c2,c3,c4,c5;
+	cout<<"ENTER 5 CHARACTERS :";
+	cin>>c1>>c2>>c3>>c4>>c5;
+	cout<<int(c1)<<" "<<int(c2)<<" "<<int(c3)<<" "<<int(c4)<<" "<<int(c5)<<endl;
+	cout<<"ENTER 5 NUMBERS :";
+	cin>>i1>>i2>>i3>>i4>>i5;
+	cout<<char(i1)<<" "<<char(i2)<<" "<<char(i3)<<" "<<char(i4)<<" "<<char(i5)<<endl;
+	
+}
